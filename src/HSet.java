a3c5d6b3c1d96239925e9da132d03a0e10c45dc7
Rafał Maziuk <ziuk.pas@gmail.com>
2021-11-02 23:17:29 +0100

    HSet commit

--- src/HSet.java
+++ src/HSet.java
@@ -6,5 +6,17 @@ public class HSet {
     public static void main(String[] args) {
 
         Set<String> set = new HashSet<>();      // set -> przechowuje unikalne wartości, bez duplikatów
+
+        set.add("pizza");
+        set.add("burger");
+        set.add("burger");
+        set.add("salad");
+        set.add("pizza");
+        set.add("pizza");
+        set.add("pizza");
+
+        for (String meal : set) {       // added everything without duplicated items, RANDOM order
+            System.out.println(meal);
+        }
     }
 }
