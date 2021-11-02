dd90928514c55c7b2ebe8dbeddf8d3bf95315f00
Rafał Maziuk <ziuk.pas@gmail.com>
2021-11-02 23:26:14 +0100

    first HMap commit

---
+++ src/HMap.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HMap {
+
+    public static void main(String[] args) {
+        // HashMap, key - value structure
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("Leo", "Football");     // to add we use PUT command
+        map.put("Xavi", "Football");
+        map.put("Many", "Box");
+        map.put("Fiodor", "MMA");
+        map.put("Adam", "Ski Jumping");
+        map.put("Alessandro", "Football");
+
+    }
+}
